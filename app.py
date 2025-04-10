20d337686da7ebf3b4b7459b0f8e144bcc57ade6
Shariq <shariqhameed127@gmail.com>
2025-04-10 21:07:57 +0500

    Code Changes

--- app.py
+++ app.py
@@ -11,27 +11,27 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def main():
     st.set_page_config("Multi PDF Chatbot", page_icon=":scroll:")
-    st.header("Multi-PDF's 📚 - Chat Agent 🤖 ")
+    st.header("Multi PDFs Chatbot - 🤖 ")
 
-    user_question = st.text_input("Ask a Question from the pdf Files uploaded...")
+    user_question = st.text_input("Ask a Question from your uploaded PDF")
 
     if user_question:
         user_input(user_question=user_question, st=st)
 
     with st.sidebar:
-        st.title("📁 PDF File's Section")
+        st.title("📁 PDF Files")
         pdf_docs = st.file_uploader(
-            "Upload your PDF Files \n & Click on the Submit & Process Button",
+            "Upload your PDF Files \n & Click Submit",
             accept_multiple_files=True,
         )
 
-        if st.button("Submit & Process"):
-            with st.spinner("Processing..."):
+        if st.button("Submit"):
+            with st.spinner("Parsing..."):
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
 
                 get_vector_store(text_chunks)
-                st.success("Done")
+                st.success("Parsing Complete")
 
         st.write("---")
 
